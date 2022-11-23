c7f4313f233098a1c70e5aec36b53b8ec4d2a762
hedon954 <171725713@qq.com>
2022-11-23 12:23:07 +0800

    add: determine encoding

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/hedon954/go-crawler
 
 go 1.16
+
+require (
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,30 +1,53 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 )
 
 func main() {
 	url := "https://www.thepaper.cn"
+
+	bs, err := Fetch(url)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(bs))
+}
+
+// Fetch reads url and returns bytes encoded with utf8
+func Fetch(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Printf("fetch url error: %v", err)
-		return
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("error status code: %v", resp.StatusCode)
-		return
+		return nil, fmt.Errorf("error status code: %v", resp.StatusCode)
 	}
 
-	bs, err := ioutil.ReadAll(resp.Body)
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DetermineEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
+
+// DetermineEncoding gets the website encoding automatically
+func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
+	bs, err := r.Peek(1024)
 	if err != nil {
-		fmt.Printf("read response body failed: %v", err)
-		return
+		return unicode.UTF8
 	}
 
-	fmt.Println("body:", string(bs))
+	e, _, _ := charset.DetermineEncoding(bs, "")
+	return e
 }
